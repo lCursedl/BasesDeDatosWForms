60643e90d4c6e58624e5bbd57cf322b5ced650c7
Gustavo Alvarez <idv18c.galvarez@uartesdigitales.edu.mx>
2020-03-19 20:36:55 -0600

    add base project

---
+++ TestWForms/TestWForms/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace TestWForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UserField = new System.Windows.Forms.TextBox();
+            this.UserLabel = new System.Windows.Forms.Label();
+            this.PassField = new System.Windows.Forms.TextBox();
+            this.PassLabel = new System.Windows.Forms.Label();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UserField
+            // 
+            this.UserField.Location = new System.Drawing.Point(113, 44);
+            this.UserField.Name = "UserField";
+            this.UserField.Size = new System.Drawing.Size(100, 20);
+            this.UserField.TabIndex = 0;
+            this.UserField.TextChanged += new System.EventHandler(this.UserField_TextChanged);
+            // 
+            // UserLabel
+            // 
+            this.UserLabel.AutoSize = true;
+            this.UserLabel.Location = new System.Drawing.Point(54, 47);
+            this.UserLabel.Name = "UserLabel";
+            this.UserLabel.Size = new System.Drawing.Size(29, 13);
+            this.UserLabel.TabIndex = 1;
+            this.UserLabel.Text = "User";
+            this.UserLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // PassField
+            // 
+            this.PassField.Location = new System.Drawing.Point(113, 71);
+            this.PassField.Name = "PassField";
+            this.PassField.PasswordChar = '*';
+            this.PassField.Size = new System.Drawing.Size(100, 20);
+            this.PassField.TabIndex = 2;
+            this.PassField.TextChanged += new System.EventHandler(this.PassField_TextChanged);
+            // 
+            // PassLabel
+            // 
+            this.PassLabel.AutoSize = true;
+            this.PassLabel.Location = new System.Drawing.Point(54, 74);
+            this.PassLabel.Name = "PassLabel";
+            this.PassLabel.Size = new System.Drawing.Size(53, 13);
+            this.PassLabel.TabIndex = 3;
+            this.PassLabel.Text = "Password";
+            this.PassLabel.Click += new System.EventHandler(this.PassLabel_Click);
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(113, 98);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.LoginButton.TabIndex = 4;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 148);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.PassLabel);
+            this.Controls.Add(this.PassField);
+            this.Controls.Add(this.UserLabel);
+            this.Controls.Add(this.UserField);
+            this.Name = "Form1";
+            this.Text = "Mi primer ventana";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UserField;
+        private System.Windows.Forms.Label UserLabel;
+        private System.Windows.Forms.TextBox PassField;
+        private System.Windows.Forms.Label PassLabel;
+        private System.Windows.Forms.Button LoginButton;
+    }
+}
+
---
+++ TestWForms/TestWForms/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestWForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UserField_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PassField_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PassLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            UserField.Text = "AWOO";
+        }
+    }
+}
